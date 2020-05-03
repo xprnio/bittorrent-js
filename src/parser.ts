90d68e929c7a8dca3b381c73d2026ab95ba9c329
Ragnar Laud <ragnar.laud@twn.ee>
2020-05-03 17:04:04 +0300

    Add type definitions for parser

--- src/parser.ts
+++ src/parser.ts
@@ -1,6 +1,60 @@
-class BepParser {
-  parseNext(data, offset, state, callback) {
-    const char = String.fromCharCode(data[offset]);
+type BepList = Array<any>;
+type BepDictionary = { [key: string]: string | number | BepList | BepDictionary };
+type ParserValue = null | string | number | BepList | KeyValuePair;
+type ParserCallback = (err?: Error, result?: ParserValue) => void;
+
+interface ParserQueueItem {
+  type: null | 'string' | 'integer' | 'list' | 'dictionary';
+  value: ParserValue;
+  length: number | null;
+  parent?: ParserListItem | ParserDictionaryItem;
+}
+
+interface ParserStringItem {
+  type: 'string';
+  value: null | string;
+  length: number | null;
+  parent?: ParserListItem | ParserDictionaryItem;
+}
+
+interface ParserIntegerItem {
+  type: 'integer';
+  stringValue: null | string;
+  value: null | number;
+  length: null;
+  parent?: ParserListItem | ParserDictionaryItem;
+}
+
+interface ParserListItem {
+  type: 'list';
+  value: null | BepList;
+  length: null | number;
+  parent?: ParserListItem | ParserDictionaryItem;
+}
+
+interface ParserDictionaryItem {
+  type: 'dictionary';
+  keyValuePairs: Array<KeyValuePair>;
+  value: null | BepDictionary;
+  length: number | null;
+  parent?: ParserListItem | ParserDictionaryItem;
+}
+
+interface KeyValuePair {
+  key?: null | string;
+  value?: null | string | number | BepList | KeyValuePair;
+}
+
+interface ParserState {
+  queue: Array<ParserQueueItem>;
+  value: null | ParserValue;
+}
+
+export class BepParser {
+  handleString() {}
+
+  parseNext(data: Buffer, offset: number, state: ParserState, callback: ParserCallback): void {
+    const char: string = String.fromCharCode(data[offset]);
     const current = state.queue[state.queue.length - 1];
     const parent = state.queue[state.queue.length - 2];
 
@@ -17,7 +71,7 @@ class BepParser {
       }
 
       if (!current.type) {
-        return callback('Invalid data');
+        return callback(new Error('Invalid data'));
       }
 
       return this.queueNext(data, offset, state, callback);
@@ -25,32 +79,36 @@ class BepParser {
 
     switch (current.type) {
       case 'string': {
-        if (current.length === null) current.length = 0;
+        const item = current as ParserStringItem;
 
-        if (current.value === null) {
-          if (char === ':') current.value = '';
-          else current.length = current.length * 10 + parseInt(char);
+        if (item.length === null) item.length = 0;
+
+        if (item.value === null) {
+          if (char === ':') item.value = '';
+          else item.length = item.length * 10 + parseInt(char);
           return this.queueNext(data, offset + 1, state, callback);
         }
 
-        current.value += char;
+        item.value += char;
 
-        if (current.value.length === current.length) {
+        if (item.value.length === item.length) {
           if (parent) {
             if (parent.type === 'list') {
-              parent.value[current.parent] = current.value;
+              const list = parent.value as BepList;
+              list.push(item.value);
             } else if (parent.type === 'dictionary') {
-              const kvp = parent.value[current.parent];
+              const dictionary = item.parent as ParserDictionaryItem;
+              const kvp: KeyValuePair = dictionary.keyValuePairs[dictionary.keyValuePairs.length - 1];
               if (!kvp.key) {
-                kvp.key = current.value;
+                kvp.key = item.value;
               } else if (!kvp.value) {
-                kvp.value = current.value;
+                kvp.value = item.value;
               } else {
                 return callback(new Error('Key value pair already filled'));
               }
             }
           } else {
-            state.value = current.value;
+            state.value = item.value;
           }
           state.queue.pop();
         }
@@ -58,54 +116,63 @@ class BepParser {
         return this.queueNext(data, offset + 1, state, callback);
       }
       case 'integer': {
-        if (current.value === null) current.value = '';
+        const item = current as ParserIntegerItem;
+
+        if (!item.stringValue) item.stringValue = '';
 
         if (char === 'e') {
-          if(current.value === '-0') return callback(new Error(`Invalid integer ${current.value}: negative zero`));
-          if (current.value.match(/^-?0\d+$/)) {
-            return callback(new Error(`Invalid integer ${current.value}: leading zero`));
+          if (item.stringValue === '-0') return callback(new Error(`Invalid integer ${ item.stringValue }: negative zero`));
+          if (item.stringValue.match(/^-?0\d+$/)) {
+            return callback(new Error(`Invalid integer ${ item.stringValue }: leading zero`));
           }
 
-          current.value = +current.value;
+          item.value = +item.stringValue;
+
           if (parent) {
             if (parent.type === 'list') {
-              parent.value[current.parent] = current.value;
+              const list = item.parent as ParserListItem;
+              list.value.push(item.value);
             } else if (parent.type === 'dictionary') {
-              const kvp = parent.value[current.parent];
+              const dictionary: ParserDictionaryItem = item.parent as ParserDictionaryItem;
+              const kvp: KeyValuePair = dictionary.keyValuePairs[dictionary.keyValuePairs.length - 1];
+
               if (!kvp.key) {
                 return callback(new Error('Invalid data: integer cannot be a dictionary key'));
               } else if (!kvp.value) {
-                kvp.value = current.value;
+                kvp.value = item.value;
               } else {
                 return callback(new Error('Key value pair already filled'));
               }
             }
           } else {
-            state.value = current.value;
+            state.value = item.value;
           }
           state.queue.pop();
           return this.queueNext(data, offset + 1, state, callback);
         }
 
-        current.value += char;
+        item.stringValue += char;
 
         return this.queueNext(data, offset + 1, state, callback);
       }
       case 'list': {
-        if (current.value === null) {
-          current.value = [];
-        }
+        const item = current as ParserListItem;
+
+        if (item.value === null) item.value = [];
 
         if (char === 'e') {
           if (parent) {
             if (parent.type === 'list') {
-              parent.value[current.parent] = current.value;
+              const list = parent as ParserListItem;
+              list.value.push(item.value);
             } else if (parent.type === 'dictionary') {
-              const kvp = parent.value[current.parent];
+              const dictionary = item.parent as ParserDictionaryItem;
+              const kvp: KeyValuePair = dictionary.keyValuePairs[dictionary.keyValuePairs.length - 1];
+
               if (!kvp.key) {
                 return callback(new Error('Invalid data: list cannot be a dictionary key'));
               } else if (!kvp.value) {
-                kvp.value = current.value;
+                kvp.value = item.value;
               } else {
                 return callback(new Error('Key value pair already filled'));
               }
@@ -113,7 +180,7 @@ class BepParser {
               return callback(new Error('Invalid parent'));
             }
           } else {
-            state.value = current.value;
+            state.value = item.value;
           }
           state.queue.pop();
           return this.queueNext(data, offset + 1, state, callback);
@@ -123,53 +190,57 @@ class BepParser {
           type: null,
           value: null,
           length: null,
-          parent: current.value.length,
+          parent: item,
         });
         return this.queueNext(data, offset, state, callback);
       }
       case 'dictionary': {
-        if (current.value === null) {
-          current.value = [];
-        }
+        const item = current as ParserDictionaryItem;
+
+        if (!item.keyValuePairs) item.keyValuePairs = [];
 
         if (char === 'e') {
           const dictionary = {};
 
-          for (let i = 0; i < current.value.length; i++) {
-            const kvp = current.value[i];
+          for (let i = 0; i < item.keyValuePairs.length; i++) {
+            const kvp = item.keyValuePairs[i];
             dictionary[kvp.key] = kvp.value;
           }
 
-          current.value = dictionary;
+          item.value = dictionary;
 
           if (parent) {
-            if(parent.type === 'dictionary') {
-              const kvp = parent.value[current.parent];
+            if (parent.type === 'list') {
+              const list = item.parent as ParserListItem;
+              list.value.push(item.value);
+            } else if (parent.type === 'dictionary') {
+              const dictionary = item.parent as ParserDictionaryItem;
+              const kvp: KeyValuePair = dictionary.keyValuePairs[dictionary.keyValuePairs.length - 1];
               if (!kvp.key) {
                 return callback(new Error('Invalid data: dictionary cannot be a dictionary key'));
               } else if (!kvp.value) {
-                kvp.value = current.value;
+                kvp.value = item.value;
               } else {
                 return callback(new Error('Key value pair already filled'));
               }
             } else {
-              parent.value[current.parent] = current.value;
+              return callback(new Error('Invalid parent'));
             }
           } else {
-            state.value = current.value;
+            state.value = item.value;
           }
           state.queue.pop();
           return this.queueNext(data, offset + 1, state, callback);
         }
 
-        if (current.value.length === 0) {
-          current.value.push({ key: null, value: null });
+        if (item.keyValuePairs.length === 0) {
+          item.keyValuePairs.push({ key: null, value: null });
         }
 
-        const kvp = current.value[current.value.length - 1];
+        const kvp = item.keyValuePairs[item.keyValuePairs.length - 1];
 
         if (kvp.key && kvp.value) {
-          current.value.push({
+          item.keyValuePairs.push({
             key: null,
             value: null,
           });
@@ -179,7 +250,7 @@ class BepParser {
           type: null,
           value: null,
           length: null,
-          parent: current.value.length - 1,
+          parent: item,
         });
 
         return this.queueNext(data, offset, state, callback);
@@ -189,18 +260,18 @@ class BepParser {
     callback(new Error('Not implemented'));
   }
 
-  queueNext(data, offset, state, callback) {
+  queueNext(data: Buffer, offset: number, state: ParserState, callback: ParserCallback) {
     setTimeout(() => this.parseNext(data, offset, state, callback), 0);
   }
 
-  async parse(data) {
-    const state = {
-      queue: [{
+  async parse(data): Promise<ParserValue> {
+    const state: ParserState = {
+      queue: [ {
         type: null,
         value: null,
         length: null,
         parent: null,
-      }],
+      } ],
       value: null,
     };
     return new Promise((resolve, reject) => {
@@ -211,5 +282,3 @@ class BepParser {
     });
   }
 }
-
-module.exports = { BepParser };
